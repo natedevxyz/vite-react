380d2dd36cfaca2a4188f9f9fb252a89d5f76634
nate-d3v <nate.d3v@gmail.com>
2023-03-14 22:29:51 -0300

    Add new component

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,8 +1,8 @@
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import { useState } from 'react';
+import ExpensesList from './ExpensesList';
 
 function Expenses(props) {
 	const [selectedYear, setSelectedYear] = useState('2021');
@@ -15,23 +15,10 @@ function Expenses(props) {
 		item => item.date.getFullYear().toString() === selectedYear
 	);
 
-	let expensesContent = <p>No expenses found</p>;
-
-	if (filteredExpenses.length > 0) {
-		expensesContent = filteredExpenses.map(expense => (
-			<ExpenseItem
-				key={expense.id}
-				title={expense.title}
-				amount={expense.amount}
-				date={expense.date}
-			/>
-		));
-	}
-
 	return (
 		<Card className="expenses">
 			<ExpensesFilter onSaveYear={saveYearHandler} selected={selectedYear} />
-			{expensesContent}
+			<ExpensesList items={filteredExpenses} />
 		</Card>
 	);
 }
